3def56a9f4592ed6230d239803c51c20a1de5d1f
leonhad@gmail.com <leonhad@gmail.com@3bebba91-15bf-1d63-dfaa-21f3475ada58>
2012-01-06 18:57:33 +0000

    fixed the error_message() usage Fixed the missing filein usage bug

---
+++ trunk/pdftools/src/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.h"
+#include <iostream>
+
+using namespace std;
+
+static wchar_t *program_name = NULL;
+
+void set_program_name(const char *name)
+{
+    program_name = new wchar_t[strlen(name) + 1];
+    mbstowcs(program_name, name, strlen(name));
+}
+
+void error_message(const wchar_t *msg)
+{
+    wcerr << program_name << L": " << msg << endl;
+}
---
+++ trunk/pdftools/src/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+extern void set_program_name(const char *name);
+extern void error_message(const wchar_t *msg);
+
+#endif

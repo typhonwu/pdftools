8b82e1e989dde0c5f1025ae20f197934164b8211
leonhad@gmail.com <leonhad@gmail.com@3bebba91-15bf-1d63-dfaa-21f3475ada58>
2012-01-11 01:09:07 +0000

    Fixed input parameters: help & version

--- trunk/pdftools/src/main.cpp
+++ trunk/pdftools/src/main.cpp
@@ -20,8 +20,10 @@ int verbose_flag = 0;
 int main(int argc, char *argv[])
 {
     int c;
+    bool error = false;
     const char *fileout = NULL;
     const char *filein = NULL;
+    const char *format = NULL;
 
     set_program_name(argv[0]);
 
@@ -29,13 +31,14 @@ int main(int argc, char *argv[])
         static struct option long_options[] = {
             {"verbose", no_argument, &verbose_flag, 1},
             {"help", no_argument, 0, 'h'},
+            {"version", no_argument, 0, 'v'},
             {"o", required_argument, 0, 'o'},
             {"format", required_argument, 0, 'f'},
             {0, 0, 0, 0}
         };
         int option_index = 0;
 
-        c = getopt_long(argc, argv, "h:o:f:",
+        c = getopt_long(argc, argv, "hvo:f:",
                 long_options, &option_index);
 
         /* Detect the end of the options. */
@@ -48,21 +51,25 @@ int main(int argc, char *argv[])
                 fileout = optarg;
             }
             break;
-
         case 'h':
-            printf("help\n");
-            break;
-
+            printf("help 2\n");
+            return EXIT_SUCCESS;
+        case 'v':
+            wcout << PACKAGE_STRING << endl;
+            wcout << "Copyright (C) 2007 Free Software Foundation, Inc." << endl;
+            wcout << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>" << endl;
+            wcout << "This is free software: you are free to change and redistribute it." << endl;
+            wcout << "There is NO WARRANTY, to the extent permitted by law." << endl;
+            return EXIT_SUCCESS;
         case 'f':
-            printf("option -f with value `%s'\n", optarg);
+            format = optarg;
             break;
-
         case '?':
-            return -1;
+            error = true;
             break;
-
         default:
-            abort();
+            error = true;
+            error_message(L"internal error");
         }
     }
 
@@ -72,11 +79,19 @@ int main(int argc, char *argv[])
     if (optind < argc) {
         wstring msg = L"unknow option";
         filein = argv[optind++];
+        error = true;
     }
     if (optind < argc) {
         error_message(L"can't parse more than one input file");
-    } else if (!filein) {
+        error = true;
+    } else if (!filein && !error) {
         error_message(L"no input file");
+        error = true;
+    }
+
+    if (error) {
+        wcout << L"Try `" << argv[0] << L" --help' for usage." << endl;
+        return -1;
     } else {
         Parser parser;
         if (!parser.open_file(filein)) {

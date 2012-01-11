7aed716ff7faa1d5ff62bc40a3a6858be0e04141
leonhad@gmail.com <leonhad@gmail.com@3bebba91-15bf-1d63-dfaa-21f3475ada58>
2012-01-11 18:06:33 +0000

    Fixed the input file parameters

--- trunk/pdftools/src/utils.h
+++ trunk/pdftools/src/utils.h
@@ -1,9 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void set_program_name(const char *name);
+void set_program_name(const wchar_t *name);
 void error_message(const wchar_t *msg);
+void verbose_message(const wchar_t *msg);
 void set_verbose_mode(const bool verbose);
 bool verbose_mode();
+wchar_t *to_unicode(const char *str);
 
 #endif

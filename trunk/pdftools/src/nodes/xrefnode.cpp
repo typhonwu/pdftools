675078af7440c4062885a6c91b18bb81097e221a
leonhad@gmail.com <leonhad@gmail.com@3bebba91-15bf-1d63-dfaa-21f3475ada58>
2012-01-13 18:02:08 +0000

    Fixed xref parser

---
+++ trunk/pdftools/src/nodes/xrefnode.cpp
@@ -0,0 +1,26 @@
+#include "xrefnode.h"
+
+XREFNode::XREFNode() : TreeNode()
+{
+    m_start_address = NULL;
+}
+
+XREFNode::~XREFNode()
+{
+    m_references.clear();
+}
+
+void XREFNode::set_start_address(long address)
+{
+    m_start_address = address;
+}
+
+void XREFNode::add_node(uint16_t id, uint16_t generation, uint32_t address, wchar_t status)
+{
+    object_reference xref;
+    xref.id = id;
+    xref.generation = generation;
+    xref.address = address;
+    xref.status = status;
+    m_references.push_back(xref);
+}
---
+++ trunk/pdftools/src/nodes/xrefnode.h
@@ -0,0 +1,28 @@
+#ifndef XREFNODE_H
+#define XREFNODE_H
+
+#include "treenode.h"
+#include <vector>
+#include <stdint.h>
+
+typedef struct {
+    uint32_t address;
+    uint16_t id;
+    uint16_t generation;
+    wchar_t status;
+} object_reference;
+
+class XREFNode : public TreeNode {
+private:
+    std::vector<object_reference> m_references;
+    long m_start_address;
+    
+public:
+    XREFNode();
+    virtual ~XREFNode();
+    
+    void add_node(uint16_t id, uint16_t generation, uint32_t address, wchar_t status);
+    void set_start_address(long address);
+};
+
+#endif

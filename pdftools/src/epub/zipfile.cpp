900b1f3c73fbc34f571209415662ed5d41a9efe7
leonhad <leonhad@gmail.com>
2012-01-30 18:25:04 +0000

    Fixed minizip support

--- pdftools/src/epub/zipfile.cpp
+++ pdftools/src/epub/zipfile.cpp
@@ -3,6 +3,7 @@
 #include "minizip/zip.h"
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -13,17 +14,21 @@ ZipFile::ZipFile()
 
     zipFile zf;
     int errclose;
-    zf = zipOpen64(filename_try, 0);
+#ifdef USEWIN32IOAPI
+    zlib_filefunc_def ffunc;
+    fill_win32_filefunc(&ffunc);
+    zf = zipOpen2(filename_try, 0, NULL, &ffunc);
+#else
+    zf = zipOpen(filename_try, 0);
+#endif
 
     if (zf == NULL) {
-        printf("error opening %s\n", filename_try);
+        cout << "error opening " << filename_try << endl;
         err = ZIP_ERRNO;
     } else
-        printf("creating %s\n", filename_try);
+        cout << "creating " << filename_try << endl;
 
-
-    FILE * fin;
-    const char* filenameinzip = "META/teste.txt";
+    const char* filenameinzip = "teste/teste.txt";
     const char *savefilenameinzip;
     zip_fileinfo zi;
 
@@ -31,35 +36,33 @@ ZipFile::ZipFile()
     while (savefilenameinzip[0] == '\\' || savefilenameinzip[0] == '/') {
         savefilenameinzip++;
     }
-
+    
     err = zipOpenNewFileInZip(zf, savefilenameinzip, &zi,
             NULL, 0, NULL, 0, NULL, Z_DEFLATED,
             Z_DEFAULT_COMPRESSION);
 
     if (err != ZIP_OK)
-        printf("error in opening %s in zipfile\n", filenameinzip);
-    
+        cout << "error in opening " << filenameinzip << " in zipfile" << endl;
+
     if (err == ZIP_OK) {
         string test = "buffer test";
         err = zipWriteInFileInZip(zf, test.c_str(), test.length());
         if (err < 0) {
-            printf("error in writing %s in the zipfile\n",
-                    filenameinzip);
+            cout << "error in writing " << filenameinzip << " in the zipfile" << endl;
         }
     }
-    
+
     if (err < 0)
         err = ZIP_ERRNO;
     else {
         err = zipCloseFileInZip(zf);
         if (err != ZIP_OK)
-            printf("error in closing %s in the zipfile\n",
-                filenameinzip);
+            cout << "error in closing " << filenameinzip << " in the zipfile" << endl;
     }
 
     errclose = zipClose(zf, NULL);
     if (errclose != ZIP_OK)
-        printf("error in closing %s\n", filename_try);
+        cout << "error in closing " << filename_try << endl;
 }
 
 ZipFile::~ZipFile()

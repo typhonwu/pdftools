34174633ccb4a540690d9ca2660bef6e7533d194
leonhad@gmail.com <leonhad@gmail.com@3bebba91-15bf-1d63-dfaa-21f3475ada58>
2012-01-31 19:31:51 +0000

    Started ZIP package

--- trunk/pdftools/src/zip/zipfile.h
+++ trunk/pdftools/src/zip/zipfile.h
@@ -2,20 +2,44 @@
 #define ZIPFILE_H
 
 #include <fstream>
+#include <vector>
+#include <stdint.h>
 
 using namespace std;
+
+struct appended_files {
+    const char *name;
+    int length;
+    uint32_t date;
+    uint32_t crc;
+    uint32_t position;
+};
 
 class ZipFile {
 private:
     ofstream m_output;
+    vector<appended_files> m_files;
     
 public:
     ZipFile();
     ~ZipFile();
     
     bool open(const char *output);
-    bool close();
+    void close();
     void add_source(const char *filename, const char *buffer, int length = 0);
+    
+private:
+    void write_central_file();
+    void write_central_directory();
+    
+    void write8(uint8_t c);
+    void write16(uint16_t c);
+    void write32(uint32_t c);
+    void write_string(const char *str);
+    
+    uint32_t current_datetime();
+    uint32_t crc32(uint8_t byte, uint32_t crc32);
+    uint32_t crc32(uint8_t *data, uint32_t size);
 };
 
 #endif
